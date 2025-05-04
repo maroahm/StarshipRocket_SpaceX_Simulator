58dc2e314dc6c97c250ee0cc15697ce187ef8015
maroahm <omarsms25@gmail.com>
2025-05-04 12:36:23 +0200

    calling the functions, procedures and tasks needed to run the program

--- src/main.adb
+++ src/main.adb
@@ -3,7 +3,30 @@ use Ada.Text_IO, Ada.Numerics.Float_Random, Ada.Calendar, Util, Rocket_Stage;
 
 procedure Main is
 
+   Catastrophic_Failure: exception;
 
+   task Flight_Computer;
+
+   task body Flight_Computer is
+      num : Natural:= 33;
+   begin
+      Gen_Failure.Init;
+      Core_Stage_Tank:= new Propellant_Tank;
+      Second_Stage_Tank := new Propellant_Tank;
+
+      Core_Stage_Tank.Init(3400.0,700.0);
+      Second_Stage_Tank.Init(1600.0,330.0);
+
+
+      Core_Stage := new Stage(num);
+      Core_Stage.Init("Super Heavy Booster", Core_Stage_Tank);
+      for I in reverse 10..1 loop
+         Printer.P(I'Image);
+         delay 1.0;
+      end loop;
+      Core_Stage.Start_Engines;
+      Printer.P("We have lift off!S");
+   end Flight_Computer;
 begin
 
    --  Insert code here.
--- src/rocket_stage.ads
+++ src/rocket_stage.ads
@@ -2,7 +2,7 @@ package Rocket_Stage is
 
    protected type Propellant_Tank is
       
-      procedure Init(O: in Float, RF: in Float);
+      procedure Init(O: in Float; RF: in Float);
       procedure D_O(T: in Float);
       procedure D_RF(T: in Float);
       function Get_P_O return Float; 
